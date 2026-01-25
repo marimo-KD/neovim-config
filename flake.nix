3a15f36ef914b342d5795f78618487694d73f170
marimo-KD <34938736+marimo-KD@users.noreply.github.com>
2026-01-26 01:20:11 +0900

    first commit

---
+++ flake.nix
@@ -0,0 +1,45 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nvf = {
+      url = "github:notashelf/nvf";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    treefmt-nix = {
+      url = "github:numtide/treefmt-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { nixpkgs, nvf, ...} @ inputs: inputs.flake-parts.lib.mkFlake {
+    inherit inputs;
+  } {
+    systems = [
+      "aarch64-darwin"
+      "aarch64-linux"
+      "x86_64-darwin"
+      "x86_64-linux"
+    ];
+    imports = [ inputs.treefmt-nix.flakeModule ];
+    perSystem = { pkgs, ... }: {
+      packages.default = (nvf.lib.neovimConfiguration {
+        inherit pkgs;
+        modules = [
+          ./neovim.nix
+        ];
+      }).neovim;
+      devShells.default = pkgs.mkShell {
+        buildInputs = with pkgs; [
+          nixd
+        ];
+      };
+      treefmt = {
+        projectRootFile = "flake.nix";
+        programs = {
+          nixfmt.enable = true;
+        };
+      };
+    };
+  };
+}

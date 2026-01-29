b89d2129375edb5090b47969e660fed38ab2b43f
marimo-KD <34938736+marimo-KD@users.noreply.github.com>
2026-01-29 23:55:04 +0900

    fix

--- fzf-lua.nix
+++ fzf-lua.nix
@@ -1,23 +1,25 @@
 {
-  fzf-lua = {
-    enable = true;
-    profile = "telescope";
-    setupOpts = {
-      ui_select = true;
+  config.vim = {
+    fzf-lua = {
+      enable = true;
+      profile = "telescope";
+      setupOpts = {
+        ui_select = true;
+      };
     };
+    keymaps = [
+      {
+        key = "<leader>f";
+        mode = "n";
+        silent = true;
+        action = "<cmd>FzfLua files<cr>";
+      }
+      {
+        key = "<leader>b";
+        mode = "n";
+        silent = true;
+        action = "<cmd>FzfLua buffers<cr>";
+      }
+    ];
   };
-  keymaps = [
-    {
-      key = "<leader>f";
-      mode = "n";
-      silent = true;
-      action = "<cmd>FzfLua files<cr>";
-    }
-    {
-      key = "<leader>b";
-      mode = "n";
-      silent = true;
-      action = "<cmd>FzfLua buffers<cr>";
-    }
-  ];
 }
